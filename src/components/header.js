9471d64a2b597665eb5a48061d7b6ef3b1d01cd4
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-21 22:12:27 +0545

    :lipstick: header design started

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,15 @@
-const Header = () => {
-    return(
-        <div>
+import { Container, Navbar } from "react-bootstrap"
 
-        </div>
-    )
+const Header = () => {
+    return <Navbar bg = "dark" variant = "dark" style={{height:80}}>
+        <Container>
+        <Navbar.Brand>
+            <a>Shopping Cart</a>
+        </Navbar.Brand>
+        </Container>
+    </Navbar>
+        
+    
 }
 
 export default Header
