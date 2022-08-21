ae7393d509455a9fec7160b9e244a15928958f0b
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-21 22:32:50 +0545

    :lipstick: cart added

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,7 @@
-import { Container, FormControl, Nav, Navbar } from "react-bootstrap"
+import {Badge,Button,Container,Dropdown, FormControl,Nav, Navbar,} from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+// import { CartState } from "../context/Context";
+
 
 const Header = () => {
     return <Navbar bg = "dark" variant = "dark" style={{height:80}}>
@@ -7,10 +10,24 @@ const Header = () => {
             <a href="/">Shopping Cart</a>
         </Navbar.Brand>
         <Navbar.Text className="search">
-            <FormControl style={{width:500}} placeholder = "Search a product"
+            <FormControl style={{width:500}} placeholder = "Search a product..."
             className="m-auto"
             />
-        </Navbar.Text>
+            </Navbar.Text>
+
+        <Nav>
+        <Dropdown alignRight>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{10}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+                <span style={{ padding: 10 }}>Cart is Empty!</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+        
         </Container>
     </Navbar>
         
