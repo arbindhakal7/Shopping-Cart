dc034932246dbf76d6c84b776146618346b2a2e7
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-27 15:03:22 +0545

    :sparkles: price and delivery added

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from "./Rating";
+
 
 const SingleProduct = ({ prod }) => {
     return <div className='prducts'>
         <Card>
-            <Card.Img variant = "top" src={ prod.image } alt={ prod.name } />
+            <Card.Img variant="top" src={prod.image} alt={prod.name} />
+            <Card.Body>
+                <Card.Title>{prod.name}</Card.Title>
+                <Card.Subtitle style={{ paddingBottom: 10 }}>
+                    <span>Nrs {prod.price.split(".")[0]}</span>
+                    {prod.fastDelivery ? (
+                        <div>Fast Delivery</div>
+                    ) : (
+                        <div>4 days delivery</div>
+                    )}
+                    <Rating rating={prod.ratings} />
+                </Card.Subtitle>
+            </Card.Body>
         </Card>
     </div>
 
