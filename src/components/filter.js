b2b2eb602b9e08cb02c6602a6d2c6fd66c901a10
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-27 13:45:29 +0545

    :zap: forms categorized

--- src/components/filter.js
+++ src/components/filter.js
@@ -4,7 +4,45 @@ export const filter = () => {
   return (
     <div className='filters'>
         <span className='title'>Filter Products</span>
-        
+        <span>
+            <Form.Check
+            inline 
+            label = "Ascending"
+            name = "group1"
+            type = "radio"
+            id = {"inline-1"}
+            />
+        </span>
+
+        <span>
+            <Form.Check
+            inline 
+            label = "Descending"
+            name = "group1"
+            type = "radio"
+            id = {"inline-2"}
+            />
+        </span>
+
+        <span>
+            <Form.Check
+            inline 
+            label = "Include out of Stock"
+            name = "group1"
+            type = "checkbox"
+            id = {"inline-3"}
+            />
+        </span>
+
+        <span>
+            <Form.Check
+            inline 
+            label = "International Delivery Only"
+            name = "group1"
+            type = "radio"
+            id = {"inline-4"}
+            />
+        </span>
     </div>
   )
 }
