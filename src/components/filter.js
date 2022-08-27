6254806f3e3b61c086dfb8b7cf4195e20b4dab06
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-27 13:40:53 +0545

    :construction: filter products being made

---
+++ src/components/filter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const filter = () => {
+  return (
+    <div className='filters'>
+        <span className='title'>Filter Products</span>
+        
+    </div>
+  )
+}
