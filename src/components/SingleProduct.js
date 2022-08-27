0cbb87b4678dc8fe4a951ac791856409437b66ed
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-27 14:37:59 +0545

    :tada: instance of images created"

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
 const SingleProduct = ({ prod }) => {
-    return <div> {prod.name} </div>
-    
+    return <div className='prducts'>
+        <Card>
+            <Card.Img variant = "top" src={ prod.img } alt={ prod.name } />
+        </Card>
+    </div>
+
 }
 
 export default SingleProduct
