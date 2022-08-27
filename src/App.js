fb7fe65fa1c2c858de19ff91e7347dee98009e71
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-27 11:26:15 +0545

    :zap: introducing routes"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Header from './components/header';
 import Home from './components/Home';
 import Cart from './components/Cart';
@@ -8,13 +8,12 @@ function App() {
   return (
     <BrowserRouter>
       <Header/>
-      <div>
-      <Route path='/' exact>
-          <Home/>
-        </Route>
-        <Route path='/cart' exact>
-          <Cart/>
-        </Route>
+      <div className='App'>
+        <Routes>
+      <Route  exact path='/' component={Home}/>
+      <Route  exact path='/cart' component={Cart}/>
+
+        </Routes>
       </div>
     </BrowserRouter>
   );
