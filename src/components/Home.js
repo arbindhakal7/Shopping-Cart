06b08d86861765c5e9024e08033b40ef9b69a8a4
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-27 13:39:01 +0545

    :art: css for productContainer and imported in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import { CartState } from '../context/context'
 import SingleProduct from './SingleProduct'
+import "./styles.css"
 
 const Home = () => {
     const { state : { products },
     } = CartState()
 
     return <div className='home'>
-        <div className='prductContainer'>
+        <div className='productContainer'>
         {products.map((prod) => {
         return <SingleProduct prod={prod} key={prod.id}/>
         })}
