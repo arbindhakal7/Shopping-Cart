2d21e3915ea84eb797c21f9d0a571390c7f997a1
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-28 22:02:10 +0545

    :zap: case add to cart

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,5 +1,9 @@
 export const cartReducer = (state, action) => { //action can be increment or decrement
     switch (action.type) { //the action will check the increments or the actions
+
+        case "ADD_TO_CART":
+            return {...state,cart:[...state.cart, {...action.payload, qty:1}]}
+
         default:
             return state;
     }
