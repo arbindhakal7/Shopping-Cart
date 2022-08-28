f9a671af28a1450c76673fa71a66c0542ff3738d
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-28 22:05:52 +0545

    :zap: case remove from cart added

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -2,7 +2,11 @@ export const cartReducer = (state, action) => { //action can be increment or dec
     switch (action.type) { //the action will check the increments or the actions
 
         case "ADD_TO_CART":
-            return {...state,cart:[...state.cart, {...action.payload, qty:1}]}
+            return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] }
+
+        case "REMOVE_FROM_CART":
+            return {
+                ...state, cart: state.cart.filter((c) => c.id !== action.payload.id),};
 
         default:
             return state;
