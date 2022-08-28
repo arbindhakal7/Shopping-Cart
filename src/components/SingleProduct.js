808218964eda4c1a60c4d4f4994d35663c09aa65
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-28 22:20:10 +0545

    :zap: add and remove from cart functionality

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap'
 import Rating from "./Rating";
+import CartState from "../context/context"
 
 
 const SingleProduct = ({ prod }) => {
+
+    const { state: { cart }, dispatch, } = CartState();
+
     return <div className='products'>
         <Card>
             <Card.Img variant="top" src={prod.image} alt={prod.name} />
@@ -18,16 +22,32 @@ const SingleProduct = ({ prod }) => {
                     )}
                     <Rating rating={prod.ratings} />
                 </Card.Subtitle>
-                <Button variant = "danger">
-                        Remove from Cart
-                </Button>
-                <Button disabled = {!prod.inStock}>
+                {cart.some((p) => p.id === prod.id) ? (
+                    <Button variant="danger" onClick={() =>
+                        dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                        })
+                    }
+                    >
+                    Remove from Cart
+                    </Button>
+                ) : (
+                    <Button
+                        onClick={() =>
+                            dispatch({
+                                type: "ADD_TO_CART",
+                                payload: prod,
+                            })
+                        }
+                        disabled={!prod.inStock}
+                    >
                         {!prod.inStock ? "Out of Stock" : "Add to Cart"}
-                </Button>
+                    </Button>
+                )}
             </Card.Body>
         </Card>
     </div>
-
 }
 
 export default SingleProduct
