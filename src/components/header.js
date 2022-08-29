711696d3ce62282aef5debdf722c9a5bdd602f81
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-29 09:19:26 +0545

    :zap: rendering the cartState in the dropdown

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
-import {Badge, Container,Dropdown, FormControl,Nav, Navbar,} from "react-bootstrap";
+import {Badge, Button, Container,Dropdown, FormControl,Nav, Navbar,} from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { CartState } from "../context/context";
 // import { CartState } from "../context/Context";
@@ -33,7 +34,41 @@ const Header = () => {
             </Dropdown.Toggle>
 
             <Dropdown.Menu style={{ minWidth: 370 }}>
+
+            {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => (
+                    <span className="cartitem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        className="cartItemImg"
+                        alt={prod.name}
+                      />
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>₹ {prod.price.split(".")[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          })
+                        }
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go To Cart
+                    </Button>
+                  </Link>
+                </>
+            ) : (
                 <span style={{ padding: 10 }}>Cart is Empty!</span>
+            )}
             </Dropdown.Menu>
         </Dropdown>
         </Nav>
