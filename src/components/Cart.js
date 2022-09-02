7fda1e8306930773a799994bbbf295d668279102
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-09-02 11:37:33 +0545

    :lipstick: sidebar and the properties

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,22 +3,29 @@ import { CartState } from "../context/context"
 
 const Cart = () => {
 
-    const{
+    const {
         state: { cart },
         dispatch
     } = CartState()
 
-    return(
+    return (
         <div className="home">
             <div className="productContainer">
                 <ListGroup>
                     {cart.map((prod) => (
-                            <span>
-                                { prod.name }
-                            </span>
-                        ))}
+                        <span>
+                            {prod.name}
+                        </span>
+                    ))}
                 </ListGroup>
-            </div>    
+            </div>
+            <div className="filters summary">
+                <span className="title">Subtotal ({cart.length}) items</span>
+                <span style={{ fontWeight: 700, fontSize: 20 }}>Total: Nrs {total}</span>
+                <Button type="button" disabled={cart.length === 0}>
+                    Proceed to Checkout
+                </Button>
+            </div>
         </div>
     )
 }
