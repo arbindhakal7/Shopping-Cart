a23c5253388409ef152670f2a8dc5597b2f9642c
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-09-05 12:53:11 +0545

    :sparkles: filter by stock delivery rating and search

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -25,7 +25,18 @@ export const productReducer = (state, action) => {
         case 'SORT_BY_PRICE':
             return { ...state, sort:action.payload }
 
+        case 'FILTER_BY_STOCK':
+            return { ...state, byStock: !state.byStock }
+    
+        case 'FILTER_BY_DELIVERY':
+            return { ...state, byFastDelivery: !state.byFastDelivery }
+    
+        case 'FILTER_BY_RATING':
+            return { ...state, byRating: action.payload }
 
+        case 'FILTER_BY_SEARCH':
+            return { ...state, searchQuery: action.payload }
+        
         default:
             return state
     }
